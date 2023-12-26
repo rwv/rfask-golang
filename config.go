dd651efa7b648871029a520ada1c1526c19344b2
Seedgou <i@zczc.cz>
2023-12-26 08:12:21 +0000

    2023.12.26

---
+++ config.go
@@ -0,0 +1,12 @@
+package main
+
+const (
+	PIN_ASK_TX = 14
+	PIN_ASK_EN = 15
+	PIN_ASK_RX = 18
+)
+
+const (
+	SAMPLE_PERIOD = 0.05
+	MAX_WAVE_LEN  = 2048
+)
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rwv/rfask-golang/device/receiver"
+	"github.com/stianeikeland/go-rpio/v4"
+)
 
 func main() {
-	// Write your code here
-	fmt.Println("Hello, playground")
+	rpio.Open()
+	defer rpio.Close()
+
+	rx := receiver.New(PIN_ASK_RX, PIN_ASK_EN, MAX_WAVE_LEN, SAMPLE_PERIOD, 3*1e6, 10*1e6)
+	count := 0
+	for {
+		count += 1
+		if wave := rx.Receive(); wave != nil {
+			fmt.Printf("%v\n", wave)
+		}
+
+		fmt.Println(count)
+	}
 }
